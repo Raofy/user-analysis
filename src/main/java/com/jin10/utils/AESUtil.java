9dcc88b27780a0279f647369cff374dc95a7be06
詹小雷 <18883179959@qq.com>
2020-04-27 15:35:19 +0800

    更新了日志打印

--- src/main/java/com/jin10/utils/AESUtil.java
+++ src/main/java/com/jin10/utils/AESUtil.java
@@ -1,11 +1,13 @@
 package com.jin10.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.codec.binary.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+@Slf4j
 public class AESUtil {
 
     public static final String INIT_VECTOR = "RandomInitVector";
@@ -44,14 +46,15 @@ public class AESUtil {
      */
     public static String decrypt(String key, String encryptedText) {
         try {
+            log.info("key:  " + key + ", encryptedText= " + encryptedText);
             IvParameterSpec iv = new IvParameterSpec(INIT_VECTOR.getBytes("UTF-8"));
             SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
             byte[] original = cipher.doFinal(Base64.decodeBase64(encryptedText));
             return new String(original);
         } catch (Exception ex) {
+            log.error("解密数据异常: " + ex);
             ex.printStackTrace();
         }
         return null;
